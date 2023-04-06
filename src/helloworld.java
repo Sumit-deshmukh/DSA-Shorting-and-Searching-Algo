c7ce89c5fb9220402d6408b8b4d6554553b408c6
Sumit deshmukh <114066570+Sumit-deshmukh@users.noreply.github.com>
2023-04-06 16:32:24 +0530

    Update helloworld.java

--- src/helloworld.java
+++ src/helloworld.java
@@ -12,12 +12,12 @@ public class helloworld {
                 arr[row][col] = input.nextInt();
             }
         }
-//        for (int row = 0; row < arr.length; row++) {
-//            for (int col = 0; col < arr[row].length; col++) {
-//                System.out.print(arr[row][col] + " ");
-//            }
-//            System.out.println();
-//        }
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                System.out.print(arr[row][col] + " ");
+            }
+            System.out.println();
+        }
         for( int row = 0; row < arr.length; row++){
             System.out.println(Arrays.toString(arr[row]));
         }
